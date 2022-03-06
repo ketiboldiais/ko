5b6f648d2565d195ea604fd5f4b3875e4bee5f7c
Ketib Oldiais <numerstack@gmail.com>
2022-03-05 18:06:35 -0600

    update contacts and resume

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 let links = document.querySelectorAll("nav a");
 for (let link of links) {
-    link.addEventListener('click', function() {
-        let sections = document.getElementsByTagName("section");
-        for (let section of sections) {
-            if ("#" + section.id === link.getAttribute("href")) {
-							setTimeout(function () { section.style.opacity = "100%" }, 500)
-                // section.style.display = "block";
-            } else {
-							setTimeout(function () { section.style.opacity = "0%" }, 500)
-                // section.style.display = "none";
-            }
-        }
-    });
+	link.addEventListener("click", function () {
+		let sections = document.getElementsByTagName("article");
+		for (let section of sections) {
+			if ("#" + section.id === link.getAttribute("href")) {
+                links.forEach(e => { 
+                    if (e.classList.contains('current')) { 
+                        e.classList.remove('current')
+                    }
+                })
+                link.classList.toggle('current')
+				setTimeout(function () {
+					section.style.display = "block";
+				}, 110);
+				setTimeout(function () {
+					section.style.opacity = "100%";
+				}, 300);
+			} else {
+				setTimeout(function () {
+					section.style.display = "none";
+				}, 100);
+				setTimeout(function () {
+					section.style.opacity = "0%";
+				}, 800);
+			}
+		}
+	});
 }
-
-
-
