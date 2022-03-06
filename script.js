57875f45ab264896809ca6dea535c23d66eca452
Ketib Oldiais <numerstack@gmail.com>
2022-03-06 01:15:45 -0600

    Change link colors

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-let links = document.querySelectorAll("nav a");
-for (let link of links) {
-	link.addEventListener("click", function () {
-		let sections = document.getElementsByTagName("article");
-		for (let section of sections) {
-			if ("#" + section.id === link.getAttribute("href")) {
-                links.forEach(e => { 
-                    if (e.classList.contains('current')) { 
-                        e.classList.remove('current')
-                    }
-                })
-                link.classList.toggle('current')
-				setTimeout(function () {
-					section.style.display = "block";
-				}, 11);
-				setTimeout(function () {
-					section.style.opacity = "100%";
-				}, 200);
-			} else {
-                setTimeout(function () {
-					section.style.opacity = "0%";
-				}, 1);
-				setTimeout(function () {
-					section.style.display = "none";
-				}, 10);
-				
+function nextSection() {
+	let links = document.querySelectorAll("nav a");
+	for (let link of links) {
+		link.addEventListener("click", function () {
+			let sections = document.getElementsByTagName("article");
+			for (let section of sections) {
+				if ("#" + section.id === link.getAttribute("href")) {
+					links.forEach((e) => {
+						if (e.classList.contains("current")) {
+							e.classList.remove("current");
+						}
+					});
+					link.classList.toggle("current");
+					setTimeout(function () {
+						section.style.display = "block";
+					}, 15);
+					setTimeout(function () {
+						section.style.opacity = "100%";
+					}, 100);
+				} else {
+					setTimeout(function () {
+						section.style.opacity = "0%";
+					}, 100);
+					setTimeout(function () {
+						section.style.display = "none";
+					}, 10);
+				}
 			}
-		}
-	});
+		});
+	}
 }
+nextSection();
+
+function greet() {
+	const date = new Date();
+	const hours = date.getHours();
+	let greet;
+
+	if (hours < 12) greet = "Good morning.";
+	else if (hours >= 12 && hours <= 17) greet = "Good afternoon.";
+	else if (hours >= 17 && hours <= 24) greet = "Good evening.";
+	else greet = "Hello.";
+
+	document.getElementById("greet").innerHTML = greet;
+}
+greet();
